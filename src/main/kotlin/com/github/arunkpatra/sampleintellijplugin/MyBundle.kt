d48056d98f95c4d31e7aaee23996ed8a874e58c0
GitHub Action <action@github.com>
2022-03-04 12:25:58 +0000

    Template cleanup

--- src/main/kotlin/com/github/arunkpatra/sampleintellijplugin/MyBundle.kt
+++ src/main/kotlin/com/github/arunkpatra/sampleintellijplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.arunkpatra.sampleintellijplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/arunkpatra/sampleintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.arunkpatra.sampleintellijplugin.services
+
+import com.github.arunkpatra.sampleintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
